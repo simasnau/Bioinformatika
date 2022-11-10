0ea50225d1a11316ef3de6e50ba6bb52179017c8
simasnau <simasnau@gmail.com>
2022-11-10 17:16:51 +0200

    Prideti naujai sugeneruoti medziai

--- 2Lab4Punktas/main.py
+++ 2Lab4Punktas/main.py
@@ -1,6 +1,6 @@
 from ete3 import Tree
 
-camel_virus = 'MT085175.1'
+camel_virus = 'lcl|Query_3460_4901-8458_MN514967.1_Dromedary_camel_coronavirus_HKU23_isolate_DcCoV-HKU23/camel/Nigeria/NV1385/2016'
 generated = Tree('treeOutput.txt', format=1)
 
 print("Prieš:")
@@ -11,3 +11,6 @@ generated.set_outgroup(camel_virus)
 
 print("Po:")
 print(generated)
+
+generated.write(format=1, outfile="modifiedTree.txt")
+generated.render("modifiedTree.png")
